cc42081f89acc973ffabd0492a0a51ecc0797a44
jangya <jangya069@gmail.com>
2017-01-30 17:38:31 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,104 +1,54 @@
-<div class="navbar-wrapper">
+<header id="header" class="header-section section section-dark navbar navbar-fixed-top">
       <div class="container">
+            <div class="navbar-header navbar-left">
 
-        <nav class="navbar navbar-inverse navbar-fixed-top header">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">
-              	<img src="assets/demo.png" alt="logo" >
-              </a>
+                  <!-- RESPONSIVE MENU BUTTON -->
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                  </button>
+
+                  <!-- HEADER LOGO -->
+                  <a class="navbar-logo navbar-brand anchor-link" href="#">
+                        <img src="assets/demo.png" alt="Spytrac">
+                  </a>
+
             </div>
-            <div id="navbar" class="navbar-collapse collapse mega-menu">
-              <ul class="nav navbar-nav navbar-right ">
-                <li class=""><a href="/">Home</a></li>
-                <li class=""><a href="/">About Us</a></li>
-                <li class=""><a href="/">Products</a></li>
-                <li class=""><a href="/">Solutions</a></li>
-                <li><a href="contact-us.php">Contact us</a></li>
-                <!-- <li class="dropdown mega-menu-fullwidth">
-                  <a href="/" class="dropdown-toggle" data-toggle="dropdown">Services <i class="fa fa-angle-down"></i></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li>
-                            <div class="mega-menu-content disable-icons">
-                              <div class="container">
-                                <div class="row">
-                                  <div class="col-menu col-md-3 equal-height-in"> 
-                                    <div class="content equal-height-list">
-                                        <ul class="menu-col list-unstyled equal-height-list">
-                                            <li><h3 class="title">WEB DESIGNING & APP DEVELOPMENT</h3></li>
-                                            <li><a href="/service.php#design">Web Development</a></li>
-                                            <li><a href="/service.php#design">Web Design</a></li>
-                                            <li><a href="/service.php#design">Web Re-developement</a></li>
-                                            <li><a href="/service.php#design">Web Re-design</a></li>
-                                            <li><a href="/service.php#design">Website Maintenance</a></li>
-                                            <li><a href="/service.php#design">Custom Web Design</a></li>
-                                            <li><a href="/service.php#design">WordPress Development</a></li>
-                                            <li><a href="/service.php#design">Website Hosting</a></li>
-                                        </ul>
-                                    </div>
-                                  </div>
-                                  <div class="col-menu col-md-4 equal-height-in">                                     
-                                    <div class="content">
-                                      <ul class="menu-col list-unstyled equal-height-list">
-                                          <li><h3 class="title">E-COMMERCE & CUSTOM SOLUTIONS</h3></li>
-                                          <li><a href="/service.php#ecommerce">E-Commerce Development & Services</a></li>
-                                          <li><a href="/service.php#ecommerce">Magneto Services</a></li>
-                                          <li><a href="/service.php#ecommerce">Travel & Booking Portal</a></li>
-                                          <li><a href="/service.php#ecommerce">Drupal Services</a></li>
-                                      </ul>
-                                    </div>
-                                  </div>
-                                  <div class="col-menu col-md-3 equal-height-in">                                     
-                                    <div class="content">
-                                      <ul class="menu-col list-unstyled equal-height-list">
-                                          <li><h3 class="title">INTEGRATED MARKETING</h3></li>
-                                          <li><a href="/service.php#marketing">SEO Services</a></li>
-                                          <li><a href="/service.php#marketing">Content Writing</a></li>
-                                          <li><a href="/service.php#marketing">Digital Marketing</a></li>
-                                          <li><a href="/service.php#marketing">PPC Advertising</a></li>
-                                          <li><a href="/service.php#marketing">Website Analytic</a></li>
-                                      </ul>
-                                    </div>
-                                  </div>    
-                                  <div class="col-menu col-md-2 equal-height-in">                                 
-                                    <div class="content">
-                                      <ul class="menu-col list-unstyled equal-height-list">
-                                        <li><h3 class="title">CORPORATE IDENTITY</h3></li>
-                                        <li><a href="/service.php#corporate">Business Logo Design</a></li>
-                                      </ul>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
+            <nav id="navigation" class="navigation navbar-collapse navbar-right collapse" aria-expanded="false" style="height: 81px;">
+						
+                  <!-- NAVIGATION LINKS -->
+                  <ul id="header-nav" class="nav navbar-nav">
+
+                        <li class="current"><a href="#hero" class="hidden">Top</a></li>
+
+                        <li class=""><a href="#about">About</a></li>
+                        <li><a href="#benefits">Benefits</a></li>
+                        <li><a href="#how-it-works">How it Works</a></li>
+                        <li><a href="#team">The Team</a></li>
+                        <li><a href="#pricing">Pricing</a></li>
+
+                        <!-- DROPDOWN MENU -->
+                        <li class="dropdown dropdown-hover">
+                              <a href="#" class="dropdown-toggle external" data-toggle="dropdown" role="button" aria-expanded="false">More <span class="caret"></span></a>
+                              <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#video" class="anchor-link external">Home</a></li>
+                                    <li><a href="#portfolio" class="anchor-link external">About Us</a></li>
+                                    <li><a href="#testimonial" class="anchor-link external">Products</a></li>
+                                    <li><a href="#faq" class="anchor-link external">Solutions</a></li>
+                                    <li><a href="#maps" class="anchor-link external">Contact Us</a></li>
+                              </ul>
                         </li>
-                    </ul>
-                </li>
-                <li class="dropdown mega-menu-autowidth">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Technologies <i class="fa fa-angle-down"></i></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">PHP</a></li>
-                    <li><a href="#">.NET</a></li>
-                    <li><a href="#">Java</a></li>
-                    <li><a href="#">All Testing</a></li>
-                    <li><a href="#">Frontend</a></li>
-                    <li><a href="#">Backend</a></li>
-                  </ul>
-                </li> -->
-                
-              </ul>  
-            </div>
-          </div>
-        </nav>
 
+                        <!-- HEADER ACTION BUTTON -->
+                        <li class="header-action-button"><a href="#" class="btn btn-primary">Action Button</a></li>
+
+                  </ul>
+
+            </nav>
       </div>
-    </div>
+</header>
 
 
    
