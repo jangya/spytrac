babc44463ad33f89c9e7db48a7cede8a691f1fc9
jangya <jangya069@gmail.com>
2017-01-30 16:57:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,19 +16,7 @@
 		<!-- Header 
 		==============================================-->
 		<?php include 'header.php'; ?>
-		<section class="content-1 mbr-parallax-background mbr-added" id="msg-box3-17" style="background-image: url(assets/img/home-bg2.jpg);overflow: hidden;padding: 100px 0px;position: relative;text-align: center;background-attachment: fixed !important;background-position: 50% 0;background-repeat: no-repeat;background-size: cover !important;">
-    <div class="mbr-overlay" style="opacity: 0.8;background-color: rgb(0, 0, 0);background: #222;bottom: 0;left: 0;position: absolute;right: 0;top: 0;z-index: 2;"></div>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-8">
-                <h2>Mobirise — an alternative to jQuery plugins</h2>
-                <div><p>The disadvantage of jQuery plugins is that you still need some knowledge of HTML, CSS and JavaScript to make them work, even if they do some of the heavy lifting for you. They also frequently limit the effects they allow you to apply.</p><p>Mobirise is a drag-and-drop website builder which allows you to create sophisticated and modern looking websites without any knowledge of coding.&nbsp;</p><p><br></p><p><strong>How to add a parallax scroll effect using Mobirise</strong>&nbsp;</p><p><br></p><p>1. Add content to your webpage by clicking on the Add Block to Page button (a cross in the bottom-left corner).</p><p>2. Once the content on your website is laid out, click on the Block Parameters icon — a small ‘gear’ icon at the top left of the screen.</p><p>3. Change the background image if desired. Tip: You can choose a background color or video if you prefer not to use an image by selecting from the options.</p><p>4. Turn parallax on or off with the button.</p><p>5. Add a color overlay if desired (this can help your text ‘pop’ from the background.</p><p>6. Click the X at the bottom to close the Block Parameters window.</p><p><br></p><p>Mobirise will now add all of the necessary code to your website to create a seamless parallax scroll that works across a wide range of browsers and devices. The whole thing takes just a few minutes to set up, and Mobirise itself is free to download and use.</p></div>
-                
-            </div>
-        </div>
-    </div>
-</section>
+		<div id="pic1" class="bg-holder" data-width="1024" data-height="768"></div>
 		<!-- Home and Intro
 		============================================== -->
 		<!--<section id="home" class="home-section hero-layout-simple section section-dark">
@@ -67,7 +55,7 @@
 		<script>
 			$(function() {
   
-			  $(".mbr-parallax-background").parallaxScroll({
+			  $(".bg-holder").parallaxScroll({
 			    friction: 0.5,
 			    direction: "vertical"
 			  });
